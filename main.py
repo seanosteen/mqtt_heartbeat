cdae3004ed8d778fec4340b89ea54b913784ecd3
Sean O'Steen <sean@tinkeringrocks.com>
2024-03-12 02:46:32 +0000

    Added uptime to the heartbeat message

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import time
 import paho.mqtt.client as paho
 import datetime
 
+startTime = datetime.datetime.now()
+
 while True:
 
     broker = sys.argv[1]
@@ -11,13 +13,13 @@ while True:
     broker_password = sys.argv[3]
     topic = "{0}/{1}".format(sys.argv[4],sys.argv[5])
 
-    client = paho.Client(paho.CallbackAPIVersion.VERSION1, sys.argv[5])
+    client = paho.Client(paho.CallbackAPIVersion.VERSION2, sys.argv[5])
     client.username_pw_set(broker_username, broker_password)
     print("Connecting to broker {0} as {1}".format(broker, broker_username))
     client.connect(broker)
-    print("publishing...")
-
-    message = '{{"timestamp": "{0}"}}'.format(datetime.datetime.now())
+    print("publishing:")
+    uptime = datetime.datetime.now() - startTime
+    message = '{{"timestamp": "{0}", "uptime":"{1}"}}'.format(datetime.datetime.now(), uptime)
 
     print(message)
     client.publish(topic, message)
